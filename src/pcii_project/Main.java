64439892c58850343c76b67b6036c9bafc8b9185
yuany85 <yuan.xu@universite-paris-saclay.fr>
2021-02-08 14:36:04 +0100

    modifier

--- src/pcii_project/Main.java
+++ src/pcii_project/Main.java
@@ -3,12 +3,13 @@ package pcii_project;
 import pcii_project.view.Window;
 import pcii_project.controls.Controls;
 import pcii_project.models.Model;
+import pcii_project.view.Display;
 
 public class Main {
 
 	
 	private Model model;
-	private Dispaly view;
+	private Display view;
 	private Controls controls;
 	
 	public Main() {
